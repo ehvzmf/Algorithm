921ac1e5394878ce42c8e80e493699121b47003f
Yuna <100411946+ehvzmf@users.noreply.github.com>
2023-07-17 21:03:28 +0900

    Update 1912.py

--- cote/1912.py
+++ cote/1912.py
@@ -1,5 +1,12 @@
 # https://www.acmicpc.net/problem/1912
-# 연속합 (백준)
+# 연속합 (백준) - DP
 
-def solution
-  return answer
+n = int(input())
+arr = list(map(int, input().split()))
+
+# arr[i]까지 고려했을 시 최대 연속합
+dp = [0] * n
+for i in range(1, n):
+  dp[i] = max(arr[i], dp[i-1] + arr[i])
+
+print(max(dp))
