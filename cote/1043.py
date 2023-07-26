81afa588eca5b6f9a31595b99533767d5b9a412e
Yuna <100411946+ehvzmf@users.noreply.github.com>
2023-07-26 14:17:41 +0900

    Complete 1043.py

--- cote/1043.py
+++ cote/1043.py
@@ -11,49 +11,47 @@ OUTPUT : 거짓말쟁이로 알려지지 않으면서 과장된 이야기를 할
 '''
 
 import sys
+input = sys.stdin.readline
 
-N, M = list(map(int, sys.stdin.readline().split()))
-# int(input())
-truth = list(map(int, sys.stdin.readline().split()))[1:]
-
-KNOW_TRUTH = 0
-# 유니온 파인드용 부모 리스트. 여기서 0번은 사람으로 사용하지 않고, 진실을 아는 사람으로 친다.
-parent = [i for i in range(n + 1)]
-for person in truth:
-    parent[person] = KNOW_TRUTH
-
-# union find 를 하는 이유는 서로 겹치지 않는 그룹 (거짓말을 모르는 그룹과 거짓말을 아는 그룹)으로 나누기 위해서다.
-def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
+def find(parent, x):
+    if x != parent[x]:
+        parent[x] = find(parent, parent[x])
     return parent[x]
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a < b:
+def union(parent, a, b, know_truth):
+    a = find(parent, a)
+    b = find(parent, b)
+    
+    if a in know_truth and b in know_truth:
+        return
+    if a in know_truth:
         parent[b] = a
+    elif b in know_truth:
+        parent[a] = b
     else:
-        parent[a] = b 
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+        
+n, m = map(int, input().split())
+know_truth = list(map(int, input().split()))[1:]
 
 parties = []
-for _ in range(m):
-    party = list(map(int, sys.stdin.readline().split()))[1:]
-    # 파티의 참석한 사람들에 대해 2명씩 union 해본다.
-    for i in range(len(party) - 1):
-        union(party[i], party[i + 1])
-    parties.append(party)
-
+parent = list(range(n+1))
 answer = 0
 
+for _ in range(m):
+    party = list(map(int, input().split()))[1:]
+    for i in range(len(party) - 1):
+        union(parent, party[i], party[i+1], know_truth)
+    parties.append(party)
+
 for party in parties:
-    know = False
     for i in range(len(party)):
-        # 진실을 알고 있던 그룹에 속했었을 경우
-        if find(party[i]) == KNOW_TRUTH:
-            know = True
+        if find(parent, party[i]) in know_truth:
             break
-    if not know:
+    else:
         answer += 1
 
 print(answer)
