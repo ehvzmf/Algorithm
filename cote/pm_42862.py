581a13167791ec8c907b24aff7c6dc0d7229e2d0
Yuna <100411946+ehvzmf@users.noreply.github.com>
2023-07-29 02:10:08 +0900

    Complete pm_42862.py
    set을 사용한 풀이

--- cote/pm_42862.py
+++ cote/pm_42862.py
@@ -8,9 +8,13 @@ OUTPUT :
 
 # 전체 학생 수 n(2~30), 체육복 도난당한 학생들의 번호 배열 lost, 여벌의 체육복 학생 번호 reserve
 def solution(n, lost, reserve):
-    answer = 0
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
     
-    for i in range(lost):
-        if lost[i-1] in reserve or lost[i+1] in reserve:
-            
+    for i in set_reserve:
+        if i-1 in set_lost:
+            set_lost.remove(i-1)
+        elif i+1 in set_lost:
+            set_lost.remove(i+1)
+    answer = n - len(set_lost)
     return answer
