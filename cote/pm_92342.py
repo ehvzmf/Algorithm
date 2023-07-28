831ce868ce835308a8c397fcceac8798c82695c5
Yuna <100411946+ehvzmf@users.noreply.github.com>
2023-07-29 02:15:50 +0900

    Create pm_92342.py

---
+++ cote/pm_92342.py
@@ -0,0 +1,12 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/92342
+프로그래머스 92342. 양궁대회 (DFS, 완전탐색, 트리)
+INPUT : 화살의 개수 n, 어피치가 맞힌 과녁 점수의 개수 10점부터 0점까지 순서대로 info[10]
+OUTPUT : 라이언이 가장 큰 점수 차이로 우승하기 위해 n발의 화살을 어떤 과녘 점수에 맞혀야 하는지 
+'''
+
+def solution(n, info):
+    for i in range(len(info)):
+        if 
+    answer = []
+    return answer
