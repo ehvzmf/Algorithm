1884a23bb0bbec964ed15ec53311dceb08a118b8
Yuna <100411946+ehvzmf@users.noreply.github.com>
2023-08-01 01:47:35 +0900

    Create pm_92343.py

---
+++ cote/pm_92343.py
@@ -0,0 +1,15 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/92343
+프로그래머스 카카오 블라인드(92343) - 양과 늑대
+'''
+
+def solution(info, edges):
+    # info - 0은 양, 1은 늑대
+    
+    for i in (info):
+        if info[i] == 0: 
+            
+            
+        else: 
+    answer = 0
+    return answer
