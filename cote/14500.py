3edb03d67dfeeaaa39235e903ccb341262ee99d6
Yuna <100411946+ehvzmf@users.noreply.github.com>
2023-08-06 18:52:30 +0900

    Complete Tetromino (boj)
    시간초과 주의하기
    dfs는 시간이 오래 걸리고
    구현으로 만들면 빠르지만 어려움.. (지저분)

---
+++ cote/14500.py
@@ -0,0 +1,44 @@
+'''
+https://www.acmicpc.net/problem/14500
+백준 14500 테트로미노
+'''
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+max_val = max(map(max, board))
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+visited = [[False] * M for _ in range(N)]
+answer = 0
+
+def dfs(x, y, step, total):
+    global answer
+    if total + max_val * (4 - step) <= answer:
+        return
+    
+    if step == 4:
+        answer = max(answer, total)
+        return
+    
+    for dx, dy in d:
+        nx = x + dx
+        ny = y + dy
+        
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+            if step == 2:
+                visited[nx][ny] = True
+                dfs(x, y, step+1, total+board[nx][ny])
+                visited[nx][ny] = False
+                
+            visited[nx][ny] = True
+            dfs(nx, ny, step+1, total+board[nx][ny])
+            visited[nx][ny] = False
+
+for i in range(N):
+    for j in range(M):
+        visited[i][j] = True
+        dfs(i, j, 1, board[i][j])
+        visited[i][j] = False
+print(answer)
