4b379e1f18b18088197dfa671eb2d91968d3b464
Yuna <100411946+ehvzmf@users.noreply.github.com>
2024-03-14 17:37:52 +0900

    Create 9466.py
    오류... 결과 출력이 한꺼번에 되지 않지만 일단 커밋

---
+++ boj/9466.py
@@ -0,0 +1,36 @@
+'''
+https://www.acmicpc.net/problem/9466
+백준 9466 - 텀 프로젝트 (dfs)
+'''
+
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(n):
+    global count
+    
+    visited[n] = True
+    cycle_list.append(n)
+    
+    if visited[arr[n]] == True:
+        if arr[n] in cycle_list:
+            count -= len(cycle_list[cycle_list.index(arr[n]):])
+        return 
+    else: 
+        dfs(arr[n])
+        
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    
+    arr = [0]
+    arr.extend([int(x) for x in sys.stdin.readline().rstrip().split()])
+    
+    visited = [False] * (N + 1)
+    count = N
+    for i in range(1, N+1):
+        if not visited[i]:
+            cycle_list = []
+            dfs(i)
+    print(count)
