9af871f2e190e20adad518c0dbb52f356763282e
mia (Yuna) <100411946+ehvzmf@users.noreply.github.com>
2024-04-22 05:30:17 +0900

    설탕 배달 (DP)
    너무 쉬운 걸 골랐나...

---
+++ boj/2839.py
@@ -0,0 +1,24 @@
+'''
+https://www.acmicpc.net/problem/2839
+백준 2839 : 설탕 배달 (DP)
+'''
+
+n = int(input())
+
+if n % 5 == 0:
+    print(n // 5)
+else: 
+    p = 0
+    while n > 0:
+        n -= 3
+        p += 1
+        if n % 5 == 0:
+            p += n // 5
+            print(p)
+            break
+        elif n == 1 or n == 2:
+            print(-1)
+            break
+        elif n == 0:
+            print(p)
+            break
