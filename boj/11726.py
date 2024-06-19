41f782ececc1e7fdbd59b8e9912cee720dc0585a
mia (Yuna) <100411946+ehvzmf@users.noreply.github.com>
2024-06-20 00:44:20 +0900

    2xn 타일링 - 역시나 기본 dp

---
+++ boj/11726.py
@@ -0,0 +1,15 @@
+'''
+https://www.acmicpc.net/problem/11726
+백준 11726 - 2xn 타일링 (dp) 
+'''
+
+n = int(input())
+
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, 1001):
+	dp[i] = (dp[i-1] + dp[i-2])% 10007
+
+print(dp[n])
