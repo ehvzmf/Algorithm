24e5b474ed47fd8876e4a195c00f12fb35417608
Yuna <yuna00708@gmail.com>
2024-10-19 03:02:40 +0900

    :sparkles: solved!

--- ACM_호텔.py
+++ ACM_호텔.py
@@ -1,11 +1,16 @@
-n = int(input())
+'''
+https://www.acmicpc.net/problem/10250
+백준 ACM 호텔 (브론즈3)
+'''
+t = int(input())
 
-for i in range(n):
+for _ in range(t):
     h, w, n = map(int, input().split())
     floor = n % h
     room = n // h + 1
-    if floor == 0:
+
+    if floor == 0: 
         room -= 1
         floor = h
-    
+
     print(floor * 100 + room)
