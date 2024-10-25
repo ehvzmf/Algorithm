a7992b79d1812d3c93d4db04e7bf59fbd3bbcad7
Yuna <yuna00708@gmail.com>
2024-10-25 23:46:41 +0900

    need to be checked

--- boj_new/좌표_압축.py
+++ boj_new/좌표_압축.py
@@ -2,7 +2,17 @@
 https://www.acmicpc.net/problem/18870
 백준 좌표 압축 (실버2) - 정렬, 값/좌표 압축
 '''
-
+'''
 n = int(input())
 arr = list(map(int, input().split()))
+'''
+import sys
 
+N = int(input())
+inputList = list(map(int,sys.stdin.readline().rstrip().split()))
+
+sortedList = sorted(list(set(inputList)))
+dictList = dict(zip(sortedList,list(range(len(sortedList)))))
+
+for x in inputList:
+    print(dictList[x])
---
+++ boj_new/회의실_배정.py
@@ -0,0 +1,4 @@
+'''
+https://www.acmicpc.net/problem/1931
+백준 1931 (실버1) - 그리디
+'''
