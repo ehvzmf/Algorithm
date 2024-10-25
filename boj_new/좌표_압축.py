c1b02ea5b33073bced2963d3b08329aa00b45f10
Yuna <yuna00708@gmail.com>
2024-10-26 06:50:21 +0900

    understood (dict)

--- boj_new/좌표_압축.py
+++ boj_new/좌표_압축.py
@@ -1,18 +1,14 @@
 '''
 https://www.acmicpc.net/problem/18870
 백준 좌표 압축 (실버2) - 정렬, 값/좌표 압축
-'''
+key: 압축된 좌표는 index값
 '''
 n = int(input())
-arr = list(map(int, input().split()))
-'''
-import sys
+inputList = list(map(int, input().split()))
 
-N = int(input())
-inputList = list(map(int,sys.stdin.readline().rstrip().split()))
-
+# 중복 제거 - set으로 만들고 다시 list로
 sortedList = sorted(list(set(inputList)))
-dictList = dict(zip(sortedList,list(range(len(sortedList)))))
+dictList = dict(zip(sortedList, list(range(len(sortedList)))))
 
 for x in inputList:
     print(dictList[x])
