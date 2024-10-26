71553659c06304c5bbd866778888eb520b245de9
Yuna <yuna00708@gmail.com>
2024-10-26 21:35:05 +0900

    :sparkles: done (:memo: lambda)

--- boj_new/회의실_배정.py
+++ boj_new/회의실_배정.py
@@ -1,4 +1,18 @@
 '''
 https://www.acmicpc.net/problem/1931
-백준 1931 (실버1) - 그리디
+백준 회의실 배정 (실버1) - 그리디
 '''
+n = int(input())
+meetings = [list(map(int, input().split())) for _ in range(n)]
+end = 0
+answer = 0
+
+# 끝나는 시간에 따라 sort
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+for a, b in meetings:
+    if end <= a: # 이전 회의 시간 <= 다음 회의 시간
+        answer += 1 
+        end = b
+
+print(answer)
