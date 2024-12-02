5e2eb923ee70af1ad0375a501458dffcf822ffa8
Yuna <yuna00708@gmail.com>
2024-12-03 02:48:36 +0900

    :sparkles: understood

--- boj_new/숫자_카드_2.py
+++ boj_new/숫자_카드_2.py
@@ -2,18 +2,36 @@
 # 백준 숫자 카드2 (실버4) - 이분탐색
 
 n = int(input())
-card = list(map(int, input().split()))
+card = sorted(list(map(int, input().split())))
 m = int(input())
 goal = list(map(int, input().split()))
 
-answer = [0] * m
-count = 0
+dic = {}
+for c in card:
+    if c in dic:
+        dic[c] += 1
+    else:
+        dic[c] = 1
 
-for i in range(m):
-    for j in range(n):
-        if goal[i] == card[j]:
-            count += 1
-    answer[i] = count
-    count = 0
+def binary(m, card, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+    if m == card[mid]:
+        return dic[m]
+    elif m < card[mid]:
+        return binary(m, card, start, mid-1)
+    else:
+        return binary(m, card, mid+1, end)
 
-print(' '.join(map(str, answer)))
+for m in goal:
+    print(binary(m, card, 0, n-1), end=' ')
+
+'''
+이분 탐색 없이 dict만 사용:
+for m in goal:
+  if m in dic:
+    print(dic[m], end=' ')
+  else:
+     print(0, end=' ')
+'''
