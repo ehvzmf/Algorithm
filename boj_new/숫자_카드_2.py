98fe41fdecd31e40a00d9a5179f552fca3144088
Yuna <yuna00708@gmail.com>
2024-12-03 02:51:37 +0900

    :see_no_evil: 이건 사기야... Counter 사용

--- boj_new/숫자_카드_2.py
+++ boj_new/숫자_카드_2.py
@@ -1,37 +1,14 @@
 # https://www.acmicpc.net/problem/10816
 # 백준 숫자 카드2 (실버4) - 이분탐색
 
+from collections import Counter
+
 n = int(input())
 card = sorted(list(map(int, input().split())))
 m = int(input())
 goal = list(map(int, input().split()))
 
-dic = {}
-for c in card:
-    if c in dic:
-        dic[c] += 1
-    else:
-        dic[c] = 1
+counter = Counter(card)
 
-def binary(m, card, start, end):
-    if start > end:
-        return 0
-    mid = (start + end) // 2
-    if m == card[mid]:
-        return dic[m]
-    elif m < card[mid]:
-        return binary(m, card, start, mid-1)
-    else:
-        return binary(m, card, mid+1, end)
-
-for m in goal:
-    print(binary(m, card, 0, n-1), end=' ')
-
-'''
-이분 탐색 없이 dict만 사용:
-for m in goal:
-  if m in dic:
-    print(dic[m], end=' ')
-  else:
-     print(0, end=' ')
-'''
+for i in goal:
+    print(counter[i], end=' ')
