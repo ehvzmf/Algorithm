aeb8540c7340ada845174935ccefa07f89e0d270
Yuna <yuna00708@gmail.com>
2024-12-28 00:14:37 +0900

    :sparkles: solved

--- boj_new/주사위_세개.py
+++ boj_new/주사위_세개.py
@@ -1,3 +1,15 @@
 # https://www.acmicpc.net/problem/2480
 # 백준 브론즈4
 
+num = list(map(int, input().split()))
+num.sort()
+answer = 0
+
+if num[0] == num[1] and num[1] == num[2]:
+    answer = 10000 + num[0] * 1000
+elif num[0] == num[1] or num[1] == num[2]:
+    answer = 1000 + num[1] * 100
+else:
+    answer += num[2] * 100
+
+print(answer)
