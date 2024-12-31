b554f855a86161188dcaa27cdb5afb6ec118ccf6
Yuna <yuna00708@gmail.com>
2024-12-31 20:41:45 +0900

    :sparkles: done

--- boj_new/블랙잭.py
+++ boj_new/블랙잭.py
@@ -3,14 +3,14 @@
 
 n, m = map(int, input().split())
 card = list(map(int, input().split()))
-card.sort(reverse=True)
-sum = card[0] + card[1]
+result = 0
 
-for i in range(2, n):
-    sum += card[i]
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if card[i] + card[j] + card[k] > m:
+                continue
+            else:
+                result = max(result, card[i] + card[j] + card[k])
 
-    if sum <= m:
-        print(sum)
-        break
-    else: 
-        sum -= card[i-2]
+print(result)
