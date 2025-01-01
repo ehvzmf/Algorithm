95794152c00414fce6cd1d98c5e2320c2da0b63c
Yuna <yuna00708@gmail.com>
2025-01-02 06:29:05 +0900

    :sparkles: solved

--- boj_new/수학은_비대면강의입니다.py
+++ boj_new/수학은_비대면강의입니다.py
@@ -1,1 +1,22 @@
 # https://www.acmicpc.net/problem/19532
+# 백준 브론즈2, 브루트포스
+
+'''
+ax + by = c
+dx + ey = f
+x = (c - by)/a = (f - ey)/d
+y = (c - ax)/b = (f - dx)/e
+'''
+
+a, b, c, d, e, f = map(int, input().split())
+
+for i in range(-999, 1000):
+    for j in range(-999, 1000):
+        if a*i + b*j == c and d*i + e*j == f:
+            print(i, j)
+
+'''
+시간 단축:
+x = int((c*e-b*f)/(a*e-b*d))
+y = int((c*d-a*f)/(b*d-a*e))
+'''
