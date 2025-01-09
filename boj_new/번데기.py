4979a0b4c7edea1e7a6813b9f34e1db061111953
Yuna <yuna00708@gmail.com>
2025-01-10 03:23:54 +0900

    :memo: today's problem

--- boj_new/번데기.py
+++ boj_new/번데기.py
@@ -1,2 +1,7 @@
 # https://www.acmicpc.net/problem/15721
-# 백준 실버5 - 브루트포스, 시뮬레이션션
+# 백준 실버5 - 브루트포스, 시뮬레이션
+
+A = int(input()) # 게임 진행하는 사람 A명
+T = int(input()) # 구하고자 하는 번째 T
+sign = int(input()) # 뻔 = 0, 데기 = 1 (예외처리도 해야 할까?)
+
