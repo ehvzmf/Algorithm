4d78534ac29ca8726528f405d0fd0d8f8d4bc4c9
Yuna <yuna00708@gmail.com>
2025-01-26 20:05:59 +0900

    check

--- boj_new/특별한_학교_탈출.py
+++ boj_new/특별한_학교_탈출.py
@@ -2,3 +2,16 @@
 # 백준 브론즈3 - 구현
 
 N, M = map(int, input().split())
+schedule = [input() for _ in range(N)]
+zipList = list(map(list, zip(*schedule)))
+
+result = 0
+for i in range(len(zipList)):
+    if zipList[i].count("X") == N:
+        result = i + 1
+        break
+
+if result > 0:
+    print(result)
+else:
+    print("ESCAPE FAILED")
