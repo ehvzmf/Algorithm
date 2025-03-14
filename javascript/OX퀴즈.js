348db06675694eacff03185a3b45e088616647c6
Yuna Choi <yuna00708@gmail.com>
2025-03-14 13:50:56 +0900

    :fire: 아주 기초적인 실수

--- javascript/OX퀴즈.js
+++ javascript/OX퀴즈.js
@@ -2,7 +2,7 @@ let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
 let num = Number(input[0]);
 
-for (let i; i <= num; i++) {
+for (let i = 1; i <= num; i++) { // [0]은 테스트 케이스의 수 
     let count = 0;
     let sum = 0;
 
